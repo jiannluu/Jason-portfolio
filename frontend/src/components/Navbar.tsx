0d5322294e36f0a4b3f0277b91619cad686c50fa
Jian Lu <jason.lu1932@gmail.com>
2023-04-25 19:26:20 -0400

    fixed bugs on navbar: on mobile, clicking navbar buttons closes menu, and fix contact me button

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -25,22 +25,26 @@ const Navbar = (): JSX.Element => {
     const windowHeight = window.innerHeight
     console.log(windowHeight)
     window.scrollTo(0, (windowHeight + 584))
+    onToggleMenu()
   }
 
   const scrollToSkill = (): void => {
     const windowHeight = window.innerHeight
     console.log(windowHeight)
     window.scrollTo(0, (windowHeight * 2 + 1334))
+    onToggleMenu()
   }
 
   const scrollToProjects = (): void => {
     const windowHeight = window.innerHeight
     console.log(windowHeight)
     window.scrollTo(0, (windowHeight * 3 + 2084))
+    onToggleMenu()
   }
 
   const scrollToContact = (): void => {
     window.scrollTo(0, document.body.scrollHeight)
+    onToggleMenu()
   }
 
   return (
@@ -77,14 +81,14 @@ const Navbar = (): JSX.Element => {
             </div>
 
             {/* hover effect for contact me button */}
-            <a href="#contact" className="hidden relative md:inline-block text-lg group mr-[4%]">
+            <button onClick={scrollToContact} className="hidden relative md:inline-block text-lg group mr-[4%]">
                 <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
                     <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
                     <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
                     <span className="relative">Contact Me</span>
                 </span>
                 <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
-            </a>
+            </button>
 
             {/* menu and menuclose button on mobile view */}
             <IconContext.Provider value={{ size: '2em' }}>
